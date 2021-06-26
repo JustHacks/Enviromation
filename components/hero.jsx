4a83d23630c2baaea0aadb60e14930e4bfeafcba
Lilykhan <47777673+Lilykhan786@users.noreply.github.com>
2021-06-26 19:53:09 +0000

    hero

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,22 +1,20 @@
 const Hero = () => {
   return (
 
-    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+    <main className="py-20 mx-auto bg-gray-900 px-4 flex justify-center items-center sm:px-6">
             <div className="text-center">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Data to enrich your</span>{' '}
-                <span className="block text-green-400 xl:inline">online business</span>
+                <span className="block text-green-400 xl:inline">Enviromation</span>
               </h1>
-              <p className="mt-3 text-base text-center text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+              <p className="my-8 text-base text-center text-gray-300 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, mollitia quis. Asperiores suscipit voluptates quis fugit quisquam dicta delectus est!
               </p>
               <div className="mt-5 sm:mt-8 sm:flex justify-center">
                 <div className="rounded-md shadow">
-                  <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-400 hover:bg-green-500 md:py-4 md:text-lg md:px-10">Get started</a>
+                  <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-400 hover:bg-green-500 md:py-4 md:text-lg md:px-10">Learn More</a>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-green-500 bg-green-100 hover:bg-green-200 md:py-4 md:text-lg md:px-10">Live demo</a>
+                  <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-green-500 bg-green-100 hover:bg-green-200 md:py-4 md:text-lg md:px-10">3D Models</a>
                 </div>
               </div>
             </div>
