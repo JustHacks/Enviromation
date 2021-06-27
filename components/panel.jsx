30c4d157fd3abd0a800e02e0aebbe0f021d6be9b
Cow <69002970+k9chelsea2@users.noreply.github.com>
2021-06-27 17:59:45 +0000

    made cards eresposnsovie

--- components/panel.jsx
+++ components/panel.jsx
@@ -1,6 +1,6 @@
 const Panel = (props) => {
 	return(
-		<div className="flex md:flex-row flex-col m-auto p-4 bg-gray-900 max-w-6xl m-auto text-gray-100 rounded-sm">
+		<div className="flex md:flex-row flex-col m-auto p-4 bg-gray-900 sm:max-w-6xl m-auto text-gray-100 rounded-sm w-full">
 			<article className="pr-4 pb-4 sm:pb-0 relative">
 				<h2 className="font-semibold text-green-400 pb-4 text-2xl">{props.header}</h2>
 				<p className="text-base text-gray-200 md:pr-4 mb-8">{props.text}</p>
