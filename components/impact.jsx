9adc4c015d61070388bbd835cc65a1c10cfd3a0d
Cow <69002970+k9chelsea2@users.noreply.github.com>
2021-06-27 09:44:22 +0000

    made panel btn only show up in impact component

--- components/impact.jsx
+++ components/impact.jsx
@@ -2,6 +2,7 @@ import Panel from './panel.jsx';
 
 const Impact = () => {
 	const modelLinkContent = "Experience the 3-D Model";
+	const linkBtn = "md:absolute bottom-0 font-medium border border-transparent rounded-md text-white bg-green-400 hover:bg-green-500 py-3 px-5";
 	return(
 		<>
 		<div className="bg-cover py-20 mx-auto px-4 text-center sm:px-6">
@@ -16,6 +17,7 @@ const Impact = () => {
 			image="https://images.unsplash.com/photo-1532393950032-b666e39c29b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80"
 			header="Dolphins"
 			text="High levels of pollution can directly kill prey species and dolphins, and completely destroy their habitat. As the top predator, river dolphins have been known to have high levels of persistent toxic chemicals in their bodies, which is likely to adversely affect their health."
+			linkBtn={ linkBtn }
 			link="https://go.echoar.xyz/TgRy"
 			linkContent={ modelLinkContent }
 			/>
@@ -23,6 +25,7 @@ const Impact = () => {
 			image="https://images.unsplash.com/photo-1533787896899-91b040188f57?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1020&q=80"
 			header="Turtles"
 			text="Sometimes even food waste from the cities and rural areas makes its way to the habitats of Turtles and other millions of sea animals .Turtles consume this food and die due to many complications.Many turtles, that have been killed by consuming debris, had plastic bags or fishing line in their stomachs, some as small as half of a fingernail. Sea turtles are especially susceptible to the effects of consuming marine debris due to their bodies’ own structure"
+			linkBtn={ linkBtn }
 			link="https://go.echoar.xyz/QgSN"
 			linkContent={ modelLinkContent }
 			/>
@@ -30,6 +33,7 @@ const Impact = () => {
 			image="https://images.unsplash.com/photo-1588522271915-53e06779abac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
 			header="Blue Whales"
 			text="All of the large whale species have been at risk of extinction due to a long history of whaling. Despite the ban on hunting, blue whales face a number of threats, all of which are caused by humans. These threats include entanglement in fishing gear,food waste, and habitat impacts including noise pollution."
+			linkBtn={ linkBtn }
 			link="https://go.echoar.xyz/J77S"
 			linkContent={ modelLinkContent }
 			/>
--- components/panel.jsx
+++ components/panel.jsx
@@ -3,10 +3,10 @@ const Panel = (props) => {
 		<div className="flex md:flex-row flex-col m-auto p-4 bg-gray-900 max-w-6xl m-auto text-gray-100 rounded-sm">
 			<article className="pr-4 pb-4 sm:pb-0 relative">
 				<h2 className="font-semibold text-green-400 pb-4 text-2xl">{props.header}</h2>
-				<p className="text-base text-gray-200 md:pr-4">{props.text}</p>
-				<a className="absolute bottom-0 text-center px-4 py-1 border border-transparent font-medium rounded-md text-white bg-green-400 hover:bg-green-500 md:py-4 md:text-lg sm:my-4 md:px-10" href={props.link} target="_blank">{props.linkContent}</a>
+				<p className="text-base text-gray-200 md:pr-4 mb-8">{props.text}</p>
+				<a className={props.linkBtn} href={props.link} target="_blank">{props.linkContent}</a>
 			</article>
-			<img className="max-w-md rounded shadow py-4 max-h-80" src={props.image} />
+			<img className="max-w-md rounded shadow py-4 sm:mt-4 max-h-80" src={props.image} />
 		</div>
 	)
 }
